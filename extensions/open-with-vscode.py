0b91389ddb4c85c0d35faaf23821a6b4f03b9f29
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-04-11 11:56:03 +0530

    Add background menu item to open current directory with Visual Studio Code

--- extensions/open-with-vscode.py
+++ extensions/open-with-vscode.py
@@ -5,6 +5,7 @@ Open the selected file or directory with Visual Studio Code
 # Imports
 from gi.repository import Nautilus, GObject
 from subprocess import call
+from typing import List
 
 # Open with VSCode Extension
 class VSCodeExtension(GObject.GObject, Nautilus.MenuProvider):
@@ -16,6 +17,20 @@ class VSCodeExtension(GObject.GObject, Nautilus.MenuProvider):
         )
         item.connect("activate", self.open_vscode, files)
         return [item]
+    
+    def get_background_items(self, current_folder: Nautilus.FileInfo) -> List[Nautilus.MenuItem]:
+        """
+        Handles the context-menu option when right-clicking an empty space in a directory
+        """
+        item = Nautilus.MenuItem(
+            name = "VSCodeExtension::OpenDirectory",
+            label = "Open with VS Code",
+            tip = "Opens the current directory with Visual Studio Code"
+        )
+        item.connect("activate", self.open_vscode, [current_folder])
+        return [item]
+
+
 
     def open_vscode(self, menu, files):
         for file in files:
